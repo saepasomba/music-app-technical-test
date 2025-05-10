766c4ca9e630c3aada758d1bc56615d29eb10ef6
Sae Pasomba <saepasomba12@gmail.com>
2025-05-10 09:07:01 +0700

    Initial Commit

---
+++ Music App-Technical Test/Music_App_Technical_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_App_Technical_TestApp.swift
+//  Music App-Technical Test
+//
+//  Created by Sae Pasomba on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Music_App_Technical_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Music App-Technical TestTests/Music_App_Technical_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_App_Technical_TestTests.swift
+//  Music App-Technical TestTests
+//
+//  Created by Sae Pasomba on 10/05/25.
+//
+
+import Testing
+@testable import Music_App_Technical_Test
+
+struct Music_App_Technical_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
